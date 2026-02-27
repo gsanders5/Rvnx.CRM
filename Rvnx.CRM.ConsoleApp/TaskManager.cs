c907023ade17f32a63ee84c682162045224f8a51
Graham Sanders <git@grahamsanders.org>
2026-02-27 12:44:19 -0600

    Code cleanup profile

--- Rvnx.CRM.ConsoleApp/TaskManager.cs
+++ Rvnx.CRM.ConsoleApp/TaskManager.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Rvnx.CRM.Core.Interfaces;
-using Rvnx.CRM.Core.Models.Contact;
 using Rvnx.CRM.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace Rvnx.CRM.ConsoleApp;
 
