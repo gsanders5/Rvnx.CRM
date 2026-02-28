61459c73aa4f00fab3829bd05b871e81a8148cfd
Graham Sanders <git@grahamsanders.org>
2026-02-28 01:41:08 -0600

    Light mode table fix

--- Rvnx.CRM.Core/Interfaces/IDebugOperationsService.cs
+++ Rvnx.CRM.Core/Interfaces/IDebugOperationsService.cs
@@ -1,33 +1,33 @@
-namespace Rvnx.CRM.Core.Interfaces;
+    namespace Rvnx.CRM.Core.Interfaces;
 
-public interface IDebugOperationsService
-{
-    /// <summary>
-    /// Merges two user accounts, transferring all data/group ownership from the second user to the first.
-    /// </summary>
-    /// <param name="user1Id">The ID of the target user (who will keep the data).</param>
-    /// <param name="user2Id">The ID of the source user (who will be deleted).</param>
-    /// <returns>A <see cref="MergeAccountsResult"/> indicating success or failure.</returns>
-    Task<MergeAccountsResult> MergeAccountsAsync(Guid user1Id, Guid user2Id);
+    public interface IDebugOperationsService
+    {
+        /// <summary>
+        /// Merges two user accounts, transferring all data/group ownership from the second user to the first.
+        /// </summary>
+        /// <param name="user1Id">The ID of the target user (who will keep the data).</param>
+        /// <param name="user2Id">The ID of the source user (who will be deleted).</param>
+        /// <returns>A <see cref="MergeAccountsResult"/> indicating success or failure.</returns>
+        Task<MergeAccountsResult> MergeAccountsAsync(Guid user1Id, Guid user2Id);
 
-    /// <summary>
-    /// Retrieves a list of all users and their group membership statistics.
-    /// </summary>
-    /// <returns>A list of <see cref="MergeUserDto"/>.</returns>
-    Task<List<MergeUserDto>> GetAllUsersWithGroupsAsync();
-}
+        /// <summary>
+        /// Retrieves a list of all users and their group membership statistics.
+        /// </summary>
+        /// <returns>A list of <see cref="MergeUserDto"/>.</returns>
+        Task<List<MergeUserDto>> GetAllUsersWithGroupsAsync();
+    }
 
-public class MergeAccountsResult
-{
-    public bool Success { get; set; }
-    public string? Message { get; set; }
-    public string? Error { get; set; }
-}
+    public class MergeAccountsResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string? Error { get; set; }
+    }
 
-public class MergeUserDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string GroupName { get; set; } = string.Empty;
-    public int GroupMemberCount { get; set; }
-}
+    public class MergeUserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
+        public int GroupMemberCount { get; set; }
+    }
